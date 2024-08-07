2f9618be637774162e6fe61790d1d3d275f2be27
miracleonyenma <miracleficient@gmail.com>
2024-08-07 13:59:51 +0100

    init: initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+document.querySelector("#app").innerHTML = `
+  <div>
+    <h1 class="variable-font">
+      I am<br>
+      Variable font<br>
+      and i'm awesome
+    </h1>
+  </div>
+`;
